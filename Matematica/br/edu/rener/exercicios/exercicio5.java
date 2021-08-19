e31cd975c8ed0944078201fcf58b11f74495896b
renerknob <renerknob2@gmail.com>
2021-08-19 01:39:28 -0300

    Teste

--- Matematica/br/edu/rener/exercicios/exercicio5.java
+++ Matematica/br/edu/rener/exercicios/exercicio5.java
@@ -1,6 +1,12 @@
 package br.edu.rener.exercicios;
 
+import java.util.Scanner;
+
 public class exercicio5 {
-	
+	public static void main (String []args){
+		Scanner scan = new Scanner(System.in);
+		
+		
+	}
 
 }
